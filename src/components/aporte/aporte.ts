865fd3110cfdb8045941d59e87e971b7fa439c80
josearangos <jose.arangos@udea.edu.co>
2017-08-20 11:02:36 -0500

    busqueda funcional con detalles
    busqueda funcional con detallitos

--- src/components/aporte/aporte.ts
+++ src/components/aporte/aporte.ts
@@ -36,46 +36,38 @@ export class AporteComponent {
     this.materia=navParams.get("materia");
     this.facultad=navParams.get("dependencia");
     this.ruta="Aportes/Dependencia/"+this.facultad+"/Materias/"+this.materia;
-    this.rutaFoto=this.ruta;
     this.aportes$=this.database.list(this.ruta);
-    this.fotos$=this.database.list(this.rutaFoto);
     this.getAportes();
 
   }
 
-
+// obtener los aportes de las personas relacionadas con las materias.
 getAportes(){
-
    this.aportes=[];
    this.temas=[];
    this.aportes$.forEach(element => {
     element.forEach(a=>{
-
         let aport:any={
         tema:a.tema,
         descripcion:a.Descripcion,
         foto:a.fotos
-        // aqui irian va las imagenes
       }
 
-      console.log(aport);
       this.aportes.push(aport);
-      this.temas.push(a.tema);
+      aport=[];
     });
  });
  this.aportes.pop();// se saca el ultimo ya que es undefied
- this.temas.pop();
 }
 
 getItems(ev: any) {
     this.getAportes();
     // set val to the value of the searchbar
     let val = ev.target.value;
-
         // if the value is an empty string don't filter the items
         if (val && val.trim() != '') {
-          this.temas = this.temas.filter((item) => {
-            return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+          this.aportes = this.aportes.filter((item) => {
+            return (item.tema.toLowerCase().indexOf(val.toLowerCase()) > -1);
           })
         }
 
